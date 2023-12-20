4b3181cf91d0fd4bf765e872bb91f67ae84f565f
TheSecurityLead <sgrie10@wgu.edu>
2023-12-20 23:55:35 +0300

    Updated error handling

--- server.js
+++ server.js
@@ -15,20 +15,31 @@ class Forecast {
   }
 }
 
-// Route for /weather
-app.get('/weather', (req, res) => {
-  const { lat, lon, searchQuery } = req.query;
-
-  const cityData = weatherData.find(city => city.lat === lat && city.lon === lon && city.city_name.toLowerCase() === searchQuery.toLowerCase());
-
-  if (!cityData) {
-    res.status(404).send('City not found');
-    return;
+function handleError(res, error) {
+    console.error(error); // Log the error for server-side debugging
+    res.status(500).send({ error: 'Something went wrong.' });
   }
 
-  const forecasts = cityData.data.map(d => new Forecast(d.valid_date, `Low of ${d.low_temp}, high of ${d.high_temp} with ${d.weather.description}`));
-  res.json(forecasts);
-});
+
+  
+// Route for /weather
+app.get('/weather', (req, res) => {
+    try {
+      const { lat, lon, searchQuery } = req.query;
+      const cityData = weatherData.find(city => city.lat === lat && city.lon === lon && city.city_name.toLowerCase() === searchQuery.toLowerCase());
+  
+      if (!cityData) {
+        res.status(404).send({ error: 'City not found' });
+        return;
+      }
+  
+      const forecasts = cityData.data.map(d => new Forecast(d.valid_date, `Low of ${d.low_temp}, high of ${d.high_temp} with ${d.weather.description}`));
+      res.json(forecasts);
+    } catch (error) {
+      handleError(res, error);
+    }
+  });
+  
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
