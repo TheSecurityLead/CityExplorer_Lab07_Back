51cb837932e44b7b3155c1c8b3ec9f569755b5b5
TheSecurityLead <sgrie10@wgu.edu>
2023-12-20 22:56:02 +0300

    First Deploy Push

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const weatherData = require('./data/weather.json');
+const app = express();
+const port = 3001;
+
+// Forecast class definition
+class Forecast {
+  constructor(date, description) {
+    this.date = date;
+    this.description = description;
+  }
+}
+
+// Route for /weather
+app.get('/weather', (req, res) => {
+  const { lat, lon, searchQuery } = req.query;
+
+  const cityData = weatherData.find(city => city.lat === lat && city.lon === lon && city.city_name.toLowerCase() === searchQuery.toLowerCase());
+
+  if (!cityData) {
+    res.status(404).send('City not found');
+    return;
+  }
+
+  const forecasts = cityData.data.map(d => new Forecast(d.valid_date, `Low of ${d.low_temp}, high of ${d.high_temp} with ${d.weather.description}`));
+  res.json(forecasts);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
